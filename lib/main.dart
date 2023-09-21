770449055abdbf42c46273371d29a0d819ef733d
Lasenko Pavel <pal.2323zet@gmail.com>
2023-09-21 13:34:41 +0300

    minor2

--- lib/main.dart
+++ lib/main.dart
@@ -6,12 +6,15 @@ import 'package:scraper_v1/pages/home_page.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-      options: const FirebaseOptions(
-    apiKey: 'AIzaSyAMhhAVfMsddJUJO8yCkeao7S5twEy2_q8',
-    appId: '1:1037175140904:web:f0e48b4e37871c378a3b1d',
-    messagingSenderId: '1037175140904',
-    projectId: 'flutter-scraper',
-  ));
+    options: const FirebaseOptions(
+      apiKey: 'AIzaSyAMhhAVfMsddJUJO8yCkeao7S5twEy2_q8',
+      appId: '1:1037175140904:web:f0e48b4e37871c378a3b1d',
+      messagingSenderId: '1037175140904',
+      projectId: 'flutter-scraper',
+      authDomain: 'flutter-scraper.firebaseapp.com',
+      storageBucket: 'flutter-scraper.appspot.com',
+    ),
+  );
   runApp(const MyApp());
 }
 
